5ccca1bd7495fe77aa3f5da130496a54f3517c3e
AswiniG23 <145984654+AswiniG23@users.noreply.github.com>
2023-10-03 23:56:14 +0530

    Add files via upload

---
+++ utilities/basePage.ts
@@ -0,0 +1,43 @@
+import { browser } from '@wdio/globals'
+import { WaitForOptions } from 'webdriverio';
+
+export default class basePage{
+
+//To open or navigate to an URL
+async open(urlPath: string){
+    await browser.url(urlPath);
+}
+
+//To set value to an text elemnt
+async setText(element: WebdriverIO.Element, text: any){
+    await this.waitForElementClickable(element, 5000, "Element not clickable");
+    await this.clickElement(element);
+    await element.setValue(text);
+   // await browser.pause(800);
+}
+
+//To click an element
+async clickElement(element: WebdriverIO.Element){
+    this.scrollIntoElement(element);
+    await element.click();
+}
+
+//To scroll to an element
+async scrollIntoElement(element: WebdriverIO.Element){
+    await element.scrollIntoView({ block: 'center', inline: 'center' });
+}
+
+//To wait for an element to exist within DOM
+async waitForElementToExist(element: WebdriverIO.Element, timeout: number, errorMessage: string){
+    const options: WaitForOptions={timeout:timeout, reverse:false, timeoutMsg: errorMessage};
+    return await element.waitForExist(options);   
+}
+
+//Waiting for an element to be clickable
+async waitForElementClickable(element: WebdriverIO.Element, timeout: number, errorMessage: string){
+    const options: WaitForOptions={timeout:timeout, reverse:false, timeoutMsg: errorMessage};
+    return await element.waitForClickable(options);   
+}
+
+
+}
